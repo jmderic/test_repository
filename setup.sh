dc333ee336f9520ca6a805c9c12c06b36113eff8
Mark Deric <codesmith@dericnet.com>
2018-01-20 13:53:38 -0800

    Add setup.sh script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+echo "# test_repository" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:jmderic/test_repository.git
+git push -u origin master
